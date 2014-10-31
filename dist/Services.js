d31c7ad0da15d29f7a71b502aefec5312a4a44cb
Enzey <enzey@enzey.net>
2014-10-30 23:33:11 -0500

    Initial Commit

---
+++ dist/Services.js
@@ -0,0 +1,37 @@
+(function (angular) {
+    "use strict";
+
+	var module = angular.module('net.enzey.services', []);
+
+	module.service('nzService', function ($document, $timeout) {
+		// position flyout
+
+		var getChildElems = function(elem) {
+			var childElems = [];
+			elem = angular.element(elem);
+			var children = elem.children();
+			for (var i=0; i < children.length; i++) {
+				getChildElems(children[i]).forEach(function(childElem) {
+					childElems.push(childElem);
+				});
+			}
+			childElems.push(elem[0]);
+
+			return childElems;
+		};
+
+		this.registerClickAwayAction = function(clickedAwayAction) {
+			var wrappedClickAwayAction  = function(event) {
+				if (getChildElems(contextObj).indexOf(event.target) === -1) {
+					$document.off('click', clickAwayAction);
+					clickedAwayAction(event);
+				}
+			};
+			$timeout(function() {
+				$document.on('click', wrappedClickAwayAction);
+			});
+		};
+
+	});
+
+});
